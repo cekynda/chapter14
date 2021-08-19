d7c14ae3ceea5c8ca38caf5078aa0aacb5ca2752
Nikita <cekynd4@gmail.com>
2021-08-19 22:51:00 +0300

    repair task4 v2

--- task4_v2.rb
+++ task4_v2.rb
@@ -4,11 +4,14 @@ require 'pry'
 class Foo
   def initialize(my_hsh)
     @my_hsh = my_hsh
-  end
-
-  @my_hsh.each do |k, v|
-    define_method k do
-      v
+    @my_hsh.each do |k, v|
+      define_singleton_method k do
+        if @my_hsh.key?(k)
+          puts v
+        else
+          puts 'нет такого ключа'
+        end
+      end
     end
   end
 end
@@ -19,5 +22,6 @@ hui = {
 }
 hui = 2
 my_hui = Foo.new(Hash[:in_Ura, 15, :in_hot_water, 25, :in_cold_water, 21])
-my_hui.qwe
+my_hui.in_cold_water
 my_hui.in_Ura
+my_hui.huila
